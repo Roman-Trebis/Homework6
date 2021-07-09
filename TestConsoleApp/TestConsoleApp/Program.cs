2cc3d8ae55d95d6a540be735ba8ed1ea577ceb0f
Roman-Trebis <85961838+Roman-Trebis@users.noreply.github.com>
2021-07-09 16:46:47 +0300

    1

---
+++ TestConsoleApp/TestConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите своё имя: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Введите свой возраст: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Твоё имя: {name}!\nВаш возраст: {age} лет.");
+
+            if (age >= 18)
+            {
+                Console.WriteLine("Вы, совершеннолетний!");
+            }
+
+            else
+            {
+                Console.WriteLine("Вы, несовершеннолетний!");
+            }
+        }
+    }
+}
